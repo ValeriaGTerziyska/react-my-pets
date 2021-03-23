0e289118ee19869ec4f04dfac5b468b311aa6f4b
Valeria Terziyska <69038113+ValeriaGTerziyska@users.noreply.github.com>
2021-03-23 23:51:48 +0200

    category components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Categories from "./components/Categories/Categories";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header />
+
+            <Switch>
+                {/* HOC  */}
+                <Route path="/" exact component={Categories}/> 
+                
+                <Route path="/categories/:category" component={Categories}/>
+            </Switch>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
